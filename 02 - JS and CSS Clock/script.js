6e017cf925d4c04ea20160a601cd75768857c8db
K750i <munkin80@gmail.com>
2018-12-19 11:41:12 +0800

    Remove transition when seconds equals 0

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -2,7 +2,7 @@ var secondHand = document.querySelector('.second-hand');
 var minuteHand = document.querySelector('.min-hand');
 var hourHand = document.querySelector('.hour-hand');
 
-function setDate() {
+function setTime() {
   // executed at the interval set by setInterval
   var now = new Date();
   var seconds = now.getSeconds();
@@ -13,11 +13,22 @@ function setDate() {
   var minsDegree = (360 / 60 * mins) + 90;
   var hoursDegree = (360 / 12 * hours) + 90;
 
+  if (seconds) {
+    secondHand.style.transitionDuration = '0.3s';
+    minuteHand.style.transitionDuration = '0.3s';
+    hourHand.style.transitionDuration = '0.3s';
+  } else {
+    secondHand.style.transitionDuration = '0s';
+    minuteHand.style.transitionDuration = '0s';
+    hourHand.style.transitionDuration = '0s';
+  }
+
   secondHand.style.transform = `rotate(${secondsDegree}deg)`;
   minuteHand.style.transform = `rotate(${minsDegree}deg)`;
   hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+  
   console.log(seconds);
-  console.log(secondsDegree)
+  console.log(secondsDegree);
 }
 
-setInterval(setDate, 1000);
+setInterval(setTime, 1000);
